29016aa5f722d2519c2b043a28a086891f397f80
omegasbk <darko.supe@fesb.hr>
2012-05-02 17:01:01 +0200

    evo ga

---
+++ content/scripts/characteristicVectors/VectorDistanceCalculator.js
@@ -0,0 +1,12 @@
+var VectorDistanceCalculator = 
+{
+	calculateHammingDistance: function(vector1, vector2)
+	{
+		return vector1 + vector2;
+	},
+	
+	calculateEucledianDistance: function(vector1, vector2)
+	{
+		
+	}
+};
