84c7963bac04db81145dd32ba159879e2fda00ef
omegasbk <darko.supe@fesb.hr>
2012-05-16 11:36:16 +0200

    Generator kombinacija vraća sve kombinacije

--- content/scripts/characteristicVectors/VectorGenerator.js
+++ content/scripts/characteristicVectors/VectorGenerator.js
@@ -1188,177 +1188,152 @@ var VectorGenerator = {
 
 function generateCombinations(endNumber, classNumber, maxDistance)
 {
-	var c = [];
-	var finalIndex = [];
-	var nodes = [];	
-	var x;
-	var numberOfCombinations = 0;
-	var numerator = 1;
-	var denominator = 1;
+	var tgd = tgd || {};
+	tgd.Math = tgd.Math || {};
+
+	tgd.Math.Combination = function (n, k, set)
+	{
+	        if (typeof tgd.Math.Combination._initialized == "undefined")
+	        {
+
+	        		tgd.Math.Combination.choose = function(n, k)
+	        		{
+	                        if (n < 0 || k < 0) { throw new Error("Invalid negative parameter in choose()"); }
+	                        if (n < k) { return 0; }
+	                        if (n == k) { return 1; }
+
+	                        var delta;
+	                        var iMax;
+	                        var ans;
+
+	                        if (k < n - k) {   delta = n - k; iMax = k;}
+	                        else {  delta = k; iMax = n - k; }
+
+	                        ans = delta + 1;
+
+	                        for (var i = 2; i <= iMax; ++i)
+	                        {
+	                            ans *= (delta + i) / i;
+	                        }
+
+	                        return ans;
+	                }
+	        }
+
+	        //"private" instance variables
+	        this._n = n;
+	        this._k = k;
+	        this._set = set.sort(function(a,b) { return a - b; });
+	        this._numCombinations = tgd.Math.Combination.choose(this._n, this._k);
+
+	        if (typeof tgd.Math.Combination._initialized == "undefined")
+	        {
+
+	                // return largest value v where v < a and  Choose(v,b) <= x
+	                tgd.Math.Combination._getLargestV = function(a, b, x)
+	                {
+	                        var v = a - 1;
+
+	                        while (tgd.Math.Combination.choose(v, b) > x)   { --v; }
+
+	                        return v;
+	                }
+
+	                //tgd original
+	                tgd.Math.Combination.prototype.getIndex = function(set)
+	                {
+	                        var retVal = 0;
+	                        var tempIdxArr = [];
+	                        var tempIdx = 0;
+
+	                        set = set.sort(function(a,b) { return a - b; });
+
+	                        for (var i = 0; i < this._set.length && tempIdx != this._k; ++i)
+	                        {
+	                                if (set[tempIdx] == this._set[i])
+	                                {
+	                                    tempIdxArr[tempIdx++] = i;
+	                                }
+	                        }
+
+	                        for (var i = 0; i < this._k; ++i)
+	                        {
+	                            tempIdxArr[i] = this._n - 1 - tempIdxArr[i];
+	                        }
+
+	                        for (var i = 0; i < this._k; ++i)
+	                        {
+	                            retVal += tgd.Math.Combination.choose(tempIdxArr[i], this._k - i);
+	                        }
+
+	                        return this._numCombinations - 1 - retVal;
+	                }
+
+	                //ported from msdn
+	                tgd.Math.Combination.prototype.element = function(m)
+	                {
+	                        var retVal = [];  //the mth lexicographic combination
+	                        var ans = [];  //used to calc the indexes into this._set
+	                        var a = this._n;
+	                        var b = this._k;
+	                        var x = this._numCombinations - 1 - m;  // x is the "dual" of m
+
+	                        for (var i = 0; i < this._k; ++i)
+	                        {
+                                ans[i] = tgd.Math.Combination._getLargestV(a, b, x);  // largest value v, where v < a and vCb < x
+                                x -= tgd.Math.Combination.choose(ans[i], b);
+                                a = ans[i];
+                                b -= 1;
+	                        }
+
+	                        for (var i = 0; i < this._k; ++i)
+	                        {
+                                ans[i] = (n - 1) - ans[i];
+	                        }
+
+	                        for (var i = 0; i < this._k; ++i)
+	                        {
+                                retVal[i] = this._set[ans[i]];
+	                        }
+
+	                        return retVal;
+	                }
+
+	                tgd.Math.Combination.prototype.toString = function()
+	                {
+	                        return this._set.join();
+	                }
+
+	                tgd.Math.Combination._initialized = true;
+	        }
+	}
+
+	var set = [];
+	
+	for(i = 0; i < endNumber; i++)
+		{
+			set.push(i);
+		}
+
+	var startTimeFirst = new Date();
+
+
+	var a = new tgd.Math.Combination(endNumber, classNumber, set);
+	var allCombinations = [];
+	
 	var brojac = 0;
-	var j;
-	var i;
-	
-	
-	
-	//napuni niz indeksima do endNumber
-	for(i = 0; i < endNumber ; i++)
-		{
-			nodes.push(i);
-		}
-	
-	
-	for (i = 0; i < classNumber; i++)
-		{
-			numerator *= (endNumber - i); 
-		}
-	
-	
-	for(i = classNumber; i > 0; i--)
-		{
-			denominator *= i;
-		}
-	
-	numberOfCombinations = numerator/denominator;
-	alert("Broj kombinacija:" + numberOfCombinations);
-	alert("Izgenerirano " + brojac + " od ukupno " + numberOfCombinations + " kombinacija!");
-	
-	
-	
-	
-	//T ALGORITAM
-	
-	
-	/*t1:
-	for(j = 1; j <= classNumber; j++ )
+
+	for(var i = 0; i < a._numCombinations; i++)
 	{
-		c.push(j - 1);
+		brojac++;
+		allCombinations.push(a.element(i));
 	}
 	
-	c[classNumber + 1] = endNumber;
-	c[classNumber + 2] = 0;
-	
-	t2:
-	for(j = classNumber; j > 0; j--)
-	{
-		x = j;
-		break t6;
-	}
-	
-	t3:
-	if ((c[1] + 1) < c[2]) 
-	{
-		c[1] = c[1] + 1;
-		for(j = classNumber; j > 0; j--)
-		{
-			x = j;
-			c[j] = x;		
-		}
-			break t2;
-	}
-	else j = 2;
-	
-	t4:
-	while( x != c[j+1] )
-	{
-		c[j-1] = j - 2;
-		x = c[j] + 1;
-		if (x == c[j+1]) j = j + 1;
-	}	
-	
-	t5: 
-		if ( j > t ) break;
-	
-	t6:
-	c[j] = x;
-	j = j - 1;
-	break t2;*/
-	
-	
-	
-	//T1
-	//fill c[] i stavi osiguraèe
-	
-	for(j = 1; j <= classNumber; j++)
-		{
-			c.push(j-1);		
-		}
-	
-	c[classNumber] = endNumber;
-	c[classNumber+1] = 0;
-	
-	j = classNumber;
-	console.log(c);
-		
-	
-	//T2
-	var stepFlag = 2;
-	
-	while (j <= classNumber)
-	{
-		if(stepFlag == 3 || stepFlag == 2)	
-		{
-			//T3
-			if (stepFlag == 3)
-				{
-					if((c[0]+1 < c[1])) 
-					{
-						c[0] = c[0] + 1;
-						stepFlag = 2;
-					}
-					else 
-					{
-						j = 2;
-						stepFlag = 4;
-					}
-				}
-			
-			//T2
-			if(stepFlag == 2)
-			{
-				if(j > 0) 
-				{
-					x = j;
-					stepFlag = 6;
-				}
-				else stepFlag = 3;
-			}									
-		}		
-		
-		
-		
-		//T4
-		if(stepFlag == 4)
-			{
-				c[j-2] = j-2;
-				x = c[j-1] + 1;
-				if ( x == c[j] ) 
-					{
-						j++;
-						stepFlag = 4;
-						
-					}
-				else stepFlag = 5;
-				
-			}
-		
-		if(stepFlag == 5)
-			{
-				if (j > classNumber)  break;
-				else stepFlag = 6;
-			}
-		
-		if(stepFlag == 6)
-			{
-				c[j-1] = x;
-				j--;
-				stepFlag = 2;
-			}
-		
-		
-	}
-	
-	
+	console.log("izgenerirano "+brojac+" kombinacija");
+
+	var endTimeFirst = new Date();
+
+	return allCombinations;
 	
 	
 	
