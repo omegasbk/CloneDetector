bed00b0bf27282119007f88fd2f4943904b44af1
Josip Maras <jomaras@gmail.com>
2012-05-23 13:20:04 +0200

    Grouping by number of tokens

--- content/scripts/characteristicVectors/CombinationsVectorCalculator.js
+++ content/scripts/characteristicVectors/CombinationsVectorCalculator.js
@@ -1,23 +1,40 @@
 var CombinationsVectorCalculator = 
 {
-		generateCombinationsVector: function(program)
-		{
-			var combinations = [];
-			var combination = [];
-			
-			combinations = MergeableNodeDetector.getChildrenCombinations(MergeableNodeDetector.getMergeableNodes(program));
-						
-			for( var i = 0; i < combinations.length; i++)
-			{
-				combination = combinations[i];
-				
-				for( var j = 0; j < combination.length; j++)
-				{
-					//ODE TI FALI DIO ZA JOINANJE VEKTORA KOMBINACIJA
-				}
-			}
-			
-			
-		}
-		
+        generateCombinationsVector: function(program)
+        {
+            var combinations = MergeableNodeDetector.getChildrenCombinations(MergeableNodeDetector.getMergeableNodes(program));
+            return combinations.map(function(combination)
+            {
+                var characteristicVector = new CharacteristicVector();
+                combination.forEach(function(item)
+                {
+                    characteristicVector.join(item.characteristicVector);
+                });
+
+                return {
+                    combination: combination,
+                    characteristicVector: characteristicVector,
+                    noOfTokens: characteristicVector.sum()
+                }
+            });
+        },
+
+        groupCombinationsVectorsByNumOfTokens: function(combinationsVectors)
+        {
+            var groups = [];
+
+            combinationsVectors.forEach(function(combinationVector)
+            {
+                if(groups[combinationVector.noOfTokens] == null) { groups[combinationVector.noOfTokens] = []; }
+
+                groups[combinationVector.noOfTokens].push(combinationVector);
+            });
+
+            return groups;
+        },
+
+        getPotentialCandidates: function(characteristicVectorGroups, maxDistance)
+        {
+
+        }
 };
