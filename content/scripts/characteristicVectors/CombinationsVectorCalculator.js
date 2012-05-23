db49c796f8de26e6cd43cf878dbef183663e3eb8
omegasbk <darko.supe@fesb.hr>
2012-05-23 18:11:31 +0200

    problem s izračunavanjem differences

--- content/scripts/characteristicVectors/CombinationsVectorCalculator.js
+++ content/scripts/characteristicVectors/CombinationsVectorCalculator.js
@@ -26,8 +26,11 @@ var CombinationsVectorCalculator =
             combinationsVectors.forEach(function(combinationVector)
             {
                 if(groups[combinationVector.noOfTokens] == null) { groups[combinationVector.noOfTokens] = []; }
-
-                groups[combinationVector.noOfTokens].push(combinationVector);
+                
+                if(combinationVector.noOfTokens != 1)
+                {
+                	groups[combinationVector.noOfTokens].push(combinationVector);
+                }
             });
 
             return groups;
@@ -35,6 +38,23 @@ var CombinationsVectorCalculator =
 
         getPotentialCandidates: function(characteristicVectorGroups, maxDistance)
         {
-
+        	var differences = [];
+        	
+        	characteristicVectorGroups.forEach(function(characteristicVectorGroup)
+        	{
+        		var group = [];
+        		
+        		characteristicVectorGroup.forEach(function(characteristicVectorItem)
+   				{
+        			group.push(characteristicVectorItem);
+   				});
+        		
+        		for(i = 0; i < group.length; i++)
+        		{
+        			if(group[i+1] != null)
+        			differences.push(VectorDistanceCalculator.calculateHammingDistance(group[i].characteristicVector, group[i+1].characteristicVector));     			
+        		}
+        	}); 	
+        	return differences;
         }
 };
