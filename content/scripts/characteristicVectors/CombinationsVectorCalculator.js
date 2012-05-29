6f34eb5b37a73b8a13bde86ee0104966c8d59543
Josip Maras <jomaras@gmail.com>
2012-05-29 08:35:17 +0200

    getting potential candidates update

--- content/scripts/characteristicVectors/CombinationsVectorCalculator.js
+++ content/scripts/characteristicVectors/CombinationsVectorCalculator.js
@@ -38,8 +38,56 @@ var CombinationsVectorCalculator =
 
         getPotentialCandidates: function(characteristicVectorGroups, maxDistance)
         {
-        	var differences = [];
-        	
+            var potentialCandidates = [];
+
+            //Go through all groups
+            for(var i = 0, groupsLength = characteristicVectorGroups.length; i < groupsLength; i++)
+            {
+                var compareWithGroups = [];
+
+                //compare with the following maxDistance groups ( from here to i+maxDistance)
+                var endGroupIndex = i + maxDistance;
+                endGroupIndex = endGroupIndex < groupsLength ? endGroupIndex : groupsLength - 1;
+
+                for(var j = i; j <= endGroupIndex; j++) { compareWithGroups.push(characteristicVectorGroups[j]); }
+
+
+                var currentGroup = characteristicVectorGroups[i];
+
+                //For each vector in the current group
+                for(j = 0, currentGroupLength = currentGroup.length; j < currentGroupLength; j++)
+                {
+                    var characteristicVector = currentGroup[j];
+
+                    //compare with vectors in the current group
+                    for(var k = j + 1; k < currentGroupLength; k++)
+                    {
+                        var compareWithCharacteristicVector = currentGroup[k];
+
+                        //How to compare if they are really similar
+                        //if() { potentialCandidates.push({first:characteristicVector, second:compareWithCharacteristicVector})};
+                    }
+
+                    //compare with all vectors in the following groups
+                    for(k = i; compareWithGroups.length; k++)
+                    {
+                        var compareWithGroup = compareWithGroups[k];
+
+                        for(var l = 0, compareGroupLength = compareWithGroup.length; l < compareGroupLength; l++)
+                        {
+                            var compareWithCharacteristicVector = compareWithGroup[l];
+
+                            //How to compare if they are really similar
+                            //if() { potentialCandidates.push({first:characteristicVector, second:compareWithCharacteristicVector})};
+                        }
+                    }
+                }
+            }
+
+            return potentialCandidates;
+
+        	/*var differences = [];
+
         	characteristicVectorGroups.forEach(function(characteristicVectorGroup)
         	{
         		var group = [];
@@ -55,6 +103,6 @@ var CombinationsVectorCalculator =
         			differences.push(VectorDistanceCalculator.calculateHammingDistance(group[i].characteristicVector, group[i+1].characteristicVector));     			
         		}
         	}); 	
-        	return differences;
+        	return differences;*/
         }
 };
